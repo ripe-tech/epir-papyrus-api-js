5fbf7ebff5bf42a5f70a81d07c7d5e9307992501
3rdvision <kazgru@gmail.com>
2020-09-30 10:53:00 +0100

    Adapt example to call getReport

--- examples/simple.js
+++ examples/simple.js
@@ -1,10 +1,13 @@
 const epirPapyrus = require("..");
+const mockOrder = require("../mocks/hermes/bastia.json");
+
+const baseUrl = "http://localhost:3000/";
 
 async function run() {
     await epirPapyrus.API.load();
-    const api = new epirPapyrus.API();
+    const api = new epirPapyrus.API({ baseUrl: baseUrl });
     try {
-        console.info(await api.info());
+        console.info(await api.getReport("hermes-silk", mockOrder));
     } catch (err) {
         console.error(`[ERROR] ${err.code || ""} ${err.message}`);
     }
